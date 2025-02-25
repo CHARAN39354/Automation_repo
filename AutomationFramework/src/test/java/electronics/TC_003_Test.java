92d2c3b8560aeb5f2f736ed1e127c94ef5f16ad3
CHARAN39354 <95952184+CHARAN39354@users.noreply.github.com>
2025-02-25 12:07:24 +0530

    Update TC_003_Test.java

--- AutomationFramework/src/test/java/electronics/TC_003_Test.java
+++ AutomationFramework/src/test/java/electronics/TC_003_Test.java
@@ -16,6 +16,7 @@ public class TC_003_Test extends BaseClass {
 		hp.getElectronicslink().click();
 		Assert.assertEquals(driver.getTitle(), "Demo Web Shop. Electronics","Electronics Page is not displayed");
 		test.log(Status.PASS,"Electronics page is displayed");
+		test.log(Status.PASS,"Electronics page is displayed");
 	}
 
 }
